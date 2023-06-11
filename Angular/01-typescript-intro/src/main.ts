c3dac09e0c6b77d5836c6e9ab043b57535413366
Steven Mendez <stevenampaiz@gmail.com>
2023-06-11 12:30:03 -0600

    feat: :sparkles: 13. Objetos, Arreglos e Interfaces

---
+++ Angular/01-typescript-intro/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+// import './topics/01-basic-types';
+import './topics/02-objects-interface';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>Hola Mundo!</h1>
+`;
