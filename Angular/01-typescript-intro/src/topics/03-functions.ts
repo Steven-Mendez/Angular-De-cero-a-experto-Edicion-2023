6f08567a0af779114e546e756887e264051aa99f
Steven Mendez <stevenampaiz@gmail.com>
2023-06-11 14:41:51 -0600

    feat: :sparkles: Funciones con objetos como argumentos

--- Angular/01-typescript-intro/src/topics/03-functions.ts
+++ Angular/01-typescript-intro/src/topics/03-functions.ts
@@ -14,10 +14,40 @@ function multiply(
   return firstNumber * base;
 }
 
-const result: string = addNumbers(1, 2).toString();
-const result2: string = addNumberArrow(1, 2);
-const multiplyResult: number = multiply(5);
+// const result: string = addNumbers(1, 2).toString();
+// const result2: string = addNumberArrow(1, 2);
+// const multiplyResult: number = multiply(5);
 
-console.log({ result, result2, multiplyResult });
+// console.log({ result, result2, multiplyResult });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number): void => {
+  character.hp += amount;
+
+  if (character.hp >= 100) {
+    character.hp = 100;
+  }
+};
+
+const strider: Character = {
+  name: 'Strider',
+  hp: 50,
+  showHp() {
+    console.log(`HP: ${this.hp}`);
+  },
+};
+
+strider.showHp();
+
+healCharacter(strider, 10);
+healCharacter(strider, 10);
+strider.showHp();
+healCharacter(strider, 50);
+strider.showHp();
 
 export {};
